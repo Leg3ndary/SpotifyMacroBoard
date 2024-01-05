5889770675cc20e48f2e68f3409d31e9663415c9
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-05 17:05:23 -0500

    Macros for pins

---
+++ src/main.ino
@@ -0,0 +1,18 @@
+#define SHUFFLE 4
+#define VOLUME_DEC 5
+#define VOLUME_INC 12
+#define LOOP 13
+#define BACK 14
+#define PAUSE_PLAY 25
+#define SKIP 26
+#define LED 18
+#define SCL 19
+#define SDA 21
+
+void setup() {
+
+}
+
+void loop() {
+
+}
