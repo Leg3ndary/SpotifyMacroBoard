0f70649c2a44ef4b8446bf3605b013eb6870b33b
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-01-14 15:41:28 -0500

    Use my own api because the other thing sucks

--- src/src.ino
+++ src/src.ino
@@ -1,24 +1,7 @@
-/*
-Plans:
-On any button press map to a func
-
-Have screen update on any button press
-
-Have LED's animate and react to button presses
-
-On each cycle do three things
-1. Check buttons
-2. Update screen
-3. Update Led's
-*/
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <ArduinoJson.h>
 #include <FastLED.h>
 #include <SPI.h>
-#include <SpotifyArduino.h>
-#include <SpotifyArduinoCert.h>
 #include <SpotifyCredentials.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
@@ -26,11 +9,10 @@ On each cycle do three things
 
 #define RGB_PIN 18
 #define RGB_LED_NUM 20
-#define BRIGHTNESS 230  // brightness range [0..255]
+#define BRIGHTNESS 230
 #define CHIP_SET WS2812B
-#define COLOR_CODE GRB  // Enum
+#define COLOR_CODE GRB
 #define UPDATES_PER_SECOND 100
-
 #define SHUFFLE 4
 #define VOLUME_DEC 5
 #define VOLUME_INC 12
@@ -41,7 +23,6 @@ On each cycle do three things
 #define LED 18
 #define SCL 19
 #define SDA 21
-
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
@@ -58,27 +39,19 @@ const unsigned char wifi_logo2[] PROGMEM = {
     0x1f, 0xe1, 0x87, 0xcf, 0xf3, 0xd8, 0x1b, 0xf1, 0x8f, 0xf3, 0xcf,
     0xfe, 0x7f, 0xfc, 0x3f, 0xfe, 0x7f, 0xff, 0xff, 0xff, 0xff};
 
-const unsigned char wifi_logo3 [] PROGMEM = {
-	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
-	0xf8, 0x1f, 0xf1, 0x8f, 0xf3, 0xcf, 0xfe, 0x7f, 0xfc, 0x3f, 0xfe, 0x7f, 0xff, 0xff, 0xff, 0xff
-};
+const unsigned char wifi_logo3[] PROGMEM = {
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xf8, 0x1f, 0xf1, 0x8f, 0xf3, 0xcf,
+    0xfe, 0x7f, 0xfc, 0x3f, 0xfe, 0x7f, 0xff, 0xff, 0xff, 0xff};
 
-WiFiClientSecure sClient;
 WiFiClientSecure bClient;
 TwoWire I2CBME = TwoWire(0);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2CBME, OLED_RESET);
 
-SpotifyArduino spotify(sClient, spotifyId, spotifySecret,
-                       SPOTIFY_REFRESH_TOKEN);
-
-unsigned long delayBetweenRequests = 10000;
-unsigned long requestDueTime;
-
 int volume = 50;
 bool shuffleState = false;
 byte repeatState = 0;
-bool playerState = false;
 int rssi = 0;
 
 const int keys[7] = {SHUFFLE, VOLUME_DEC, VOLUME_INC, LOOP,
@@ -95,6 +68,7 @@ byte a, b, c;
 // WIFI STUFF
 unsigned long nextRSSICheck = 0;
 unsigned long RSSIDelay = 10000;
+int lastRSSI = 0;
 
 void setup() {
     for (int i = 0; i < 7; i++) {
@@ -106,14 +80,14 @@ void setup() {
     pinMode(SDA, INPUT_PULLUP);
 
     Serial.begin(115200);
-    delay(1500);
+    delay(2000);
     I2CBME.begin(SDA, SCL, 400000);
-
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
         Serial.println(F("SSD1306 allocation failed"));
         for (;;)
             ;  // Don't proceed, loop forever
     }
+    delay(2000);
 
     display.clearDisplay();
 
@@ -133,15 +107,22 @@ void setup() {
     display.println("Connected to WiFi");
     display.display();
 
-    sClient.setCACert(spotify_server_cert);
     bClient.setCACert(benzServerCert);
 
-    if (!spotify.refreshAccessToken()) {
-        Serial.println("Failed to get access tokens");
-    }
+    // Finished All of the setup, now acc say starting
+    delay(2000);
+    display.clearDisplay();
+    drawScreenOutline();
+    display.setCursor(0, 16);
+    display.setTextSize(2);
+    display.println("  Starting  ");
+    display.display();
+    updateCurrent();
+    Serial.println("everything should be working");
 }
 
 void drawScreenOutline() {
+    if (lastRSSI == rssi) return;
     display.setCursor(0, 0);
     display.setTextColor(WHITE);
     display.setTextWrap(false);
@@ -153,11 +134,11 @@ void drawScreenOutline() {
             display.drawPixel(i, j, WHITE);
         }
     }
-    // for (int i = 2; i < 18; i++) {
-    //     for (int j = 0; j < 16; j++) {
-    //         display.drawPixel(i, j, BLACK);
-    //     }
-    // }
+    for (int i = 2; i < 18; i++) {
+        for (int j = 0; j < 16; j++) {
+            display.drawPixel(i, j, BLACK);
+        }
+    }
     for (int i = 18; i < 128; i++) {
         for (int j = 0; j < 16; j++) {
             display.drawPixel(i, j, WHITE);
@@ -175,21 +156,76 @@ void drawScreenOutline() {
     display.display();
 }
 
-void getColor(CurrentlyPlaying current) {
+void extractValue(const String& key, const String& json, String& result) {
+    String keyWithQuotes = "\"" + key + "\":";
+    int start = json.indexOf(keyWithQuotes);
+    if (start != -1) {
+        int end = json.indexOf(",", start + keyWithQuotes.length());
+        if (end == -1) {
+            end = json.indexOf("}", start + keyWithQuotes.length());
+        }
+        result = json.substring(start + keyWithQuotes.length(), end);
+        result.trim();
+        result.remove(0, 1);
+        result.remove(result.length() - 1);
+    }
+    yield();
+}
+
+/* Actions are as follows
+p = playPause
+b = back
+s = skip
+r = repeat
+v = volume
+    0 = decrease
+    1 = increase
+l = loop
+f = shuffle
+*/
+void updateState(char action, int subAction = 0) {
+    String actionString = "";
+    if (action == 'p') {
+        actionString = "playPause";
+    } else if (action == 'b') {
+        actionString = "back";
+    } else if (action == 's') {
+        actionString = "skip";
+    } else if (action == 'r') {
+        actionString = "repeat";
+    } else if (action == 'v') {
+        if (subAction == 0) {
+            actionString = "vinc";
+        } else {
+            actionString = "vinc";
+        }
+    } else if (action == 'l') {
+        actionString = "loop";
+    } else if (action == 'f') {
+        actionString = "shuffle";
+    }
+    unsigned long timeout = millis();
+    if (!bClient.connect("benzhou.tech", 443)) {
+        Serial.println("Connection failed");
+        return;
+    }
+    bClient.print("GET /api/manageState/" + PASSWORD + "/" + actionString +
+                  " HTTP/1.1\r\n" + "Host: benzhou.tech\r\n" +
+                  "Connection: close\r\n\r\n");
+    Serial.println("called with ");
+    Serial.println(actionString);
+    bClient.flush();
+    bClient.stop();
+}
+
+void updateCurrent() {
     if (!bClient.connect("benzhou.tech", 443)) {
         Serial.println("Connection failed");
         return;
     }
 
-    String albumImageUrl = current.albumImages[0].url;
-    int lastIndex = albumImageUrl.lastIndexOf('/');
-    String imageUrl = albumImageUrl.substring(lastIndex + 1);
-
-    String url = "/api/getColor/";
-    url += imageUrl;
-
-    bClient.print("GET " + url + " HTTP/1.1\r\n" + "Host: benzhou.tech\r\n" +
-                  "Connection: close\r\n\r\n");
+    bClient.print("GET /api/getCurrent/" + PASSWORD + " HTTP/1.1\r\n" +
+                  "Host: benzhou.tech\r\n" + "Connection: close\r\n\r\n");
 
     unsigned long timeout = millis();
     while (!bClient.available()) {
@@ -210,75 +246,78 @@ void getColor(CurrentlyPlaying current) {
         char c = bClient.read();
         response += c;
     }
-    // Serial.println(response);
+    Serial.println(response);
     bClient.flush();
+
+    String title, artist, album, color, duration, progress, pausedRaw,
+        volumeRaw, repeatRaw, shuffleRaw;
+
+    extractValue("title", response, title);
+    extractValue("artist", response, artist);
+    extractValue("album", response, album);
+    extractValue("duration", response, duration);
+    extractValue("progress", response, progress);
+    extractValue("paused", response, pausedRaw);
+    extractValue("volume", response, volumeRaw);
+    extractValue("loop", response, repeatRaw);
+    extractValue("shuffle", response, shuffleRaw);
+
+    bool paused = pausedRaw == "true";
+    int volume = volumeRaw.toInt();
+    bool shuffle = shuffleRaw == "true";
+
+    color =
+        response.substring(response.indexOf("[") + 1, response.indexOf("]"));
+
+    Serial.println(color);
+
+    int commaIndex1 = color.indexOf(",");
+    int commaIndex2 = color.indexOf(",", commaIndex1 + 1);
+
+    int red = color.substring(0, commaIndex1 + 1).toInt();
+    int green = color.substring(commaIndex1 + 1, commaIndex2 + 1).toInt();
+    int blue = color.substring(commaIndex2 + 1, color.length()).toInt();
+
+    for (int i = 0; i < RGB_LED_NUM; i++) LEDs[i] = CRGB(red, green, blue);
+    FastLED.show();
 
     display.clearDisplay();
     drawScreenOutline();
+    display.setTextColor(WHITE);
     display.setCursor(0, 16);
     display.setTextSize(2);
-    display.println(current.trackName);
+    display.println(title);
     display.setTextSize(1);
-    display.println(current.artists[0].artistName);
+    display.println(artist);
+    delay(500);
     display.display();
     display.startscrollright(2, 5);
-
-    int bracketIndex = response.indexOf('[');
-    int firstCommaIndex = response.indexOf(',');
-    int secondCommaIndex = response.indexOf(',', firstCommaIndex + 1);
-
-    int red = response.substring(bracketIndex + 1, firstCommaIndex).toInt();
-    int green =
-        response.substring(firstCommaIndex + 1, secondCommaIndex).toInt();
-    int blue =
-        response.substring(secondCommaIndex + 1, response.length() - 1).toInt();
-    for (int i = 0; i < RGB_LED_NUM; i++) LEDs[i] = CRGB(red, green, blue);
-    FastLED.show();
+    bClient.stop();
 }
 
-void shuffle() {
-    shuffleState = !shuffleState;
-    spotify.toggleShuffle(shuffleState);
-}
+void shuffle() { updateState('f'); }
 
-void volumeDecrease() {
-    volume = max(volume - 5, 0);
-    spotify.setVolume(volume);
-}
+void volumeDecrease() { updateState('v', 0); }
 
-void volumeIncrease() {
-    volume = min(volume + 5, 100);
-    spotify.setVolume(volume);
-}
+void volumeIncrease() { updateState('v', 1); }
 
 void repeat() {
-    repeatState = (repeatState + 1) % 3;
-    if (repeatState == 0) {
-        spotify.setRepeatMode(repeat_off);
-    } else if (repeatState == 1) {
-        spotify.setRepeatMode(repeat_track);
-    } else {
-        spotify.setRepeatMode(repeat_context);
-    }
+    updateState('r');
 }
 
 void back() {
-    spotify.previousTrack();
-    spotify.getCurrentlyPlaying(getColor, SPOTIFY_MARKET);
+    updateState('b');
+    updateCurrent();
 }
 
 void pausePlay() {
-    playerState = !playerState;
-    if (playerState) {
-        spotify.play();
-    } else {
-        spotify.pause();
-    }
+    updateState('p');
+    updateCurrent();
 }
 
 void skip() {
-    spotify.nextTrack();
-    spotify.getCurrentlyPlaying(getColor, SPOTIFY_MARKET);
+    updateState('s');
+    updateCurrent();
 }
 
 void (*funcs[7])() = {
